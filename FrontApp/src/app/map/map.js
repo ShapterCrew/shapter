243f4a59970a3a1f62763ff6a3991eac93b872e0
alexandre_herve <alexandre.herve9@gmail.com>
2014-09-09 14:51:34 +0200

    internship custom template beginning

--- FrontApp/src/app/map/map.js
+++ FrontApp/src/app/map/map.js
@@ -14,20 +14,42 @@ angular.module( 'shapter.maps', [
   };
 }])
 
-.controller('MapsCtrl', ['$scope', function( $scope ){
+// create a directive that replaces the popup content and add its own scope with marker stuff 
+
+.controller('MapsCtrl', ['$scope', '$compile', '$document', function( $scope, $compile, $document ){
 
   $scope.defaults = {
   };
+
+  $scope.test = 'haha';
 
   $scope.markers = {
     osloMarker: {
       lat: 59.91,
       lng: 10.75,
-      message: "I want to travel here!",
-      focus: true,
+      message: '<div ng-include=\"\'map/message.tpl.html\'\"></div>',
+      focus: false,
       draggable: false
     }
   };
+
+  $scope.$on('leafletDirectiveMarker.click', function(e, args) {
+    // Args will contain the marker name and other relevant information
+    console.log("Leaflet Click");
+    var elem = document.getElementsByClassName('leaflet-popup-content');
+    $compile( elem )($scope);
+  });
+
+  $scope.$on('leafletDirectiveMarker.popupopen', function(e, args) {
+    // Args will contain the marker name and other relevant information
+    console.log("Leaflet Popup Open");
+  });
+
+  $scope.$on('leafletDirectiveMarker.popupclose', function(e, args) {
+    // Args will contain the marker name and other relevant information
+    console.log("Leaflet Popup Close");
+  });
+
 
 }]);
 
