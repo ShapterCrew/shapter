15e1983647f9dbc09abf9d9bf17547f5b0a95ce8
alexandre_herve <alexandre.herve9@gmail.com>
2014-09-10 10:44:08 +0200

    internships makers filter

--- FrontApp/src/app/map/map.js
+++ FrontApp/src/app/map/map.js
@@ -15,27 +15,51 @@ angular.module( 'shapter.maps', [
 }])
 
 // create a directive that replaces the popup content and add its own scope with marker stuff 
-
-.controller('MapsCtrl', ['$scope', '$compile', '$document', function( $scope, $compile, $document ){
-
-  $scope.defaults = {
+.directive('shMapMessage', [function(){
+  return {
+    restrict: 'A',
+    templateUrl: 'map/message.tpl.html',
+    scope: {
+      marker: '=',
+      markers: '='
+    }
   };
+}])
 
-  $scope.test = 'haha';
+.filter('formatMarkers', [function(){
+  return function( markers ){
+    angular.forEach( markers, function( element, index ){
+      element.message = '<div sh-map-message marker=\"\'' + index + '\'\" markers=\"markers\"></div>';
+      console.log( element );
+    });
+    return markers;
+  };
+}])
 
-  $scope.markers = {
+.controller('MapsCtrl', ['$scope', '$compile', '$filter', function( $scope, $compile, $filter ){
+
+  $scope.markers = $filter( 'formatMarkers' )({
+    olsoMarker: {
+      lat: 39.91,
+      lng: 15.75,
+      message: '',
+      focus: false,
+      draggable: false
+    },
     osloMarker: {
       lat: 59.91,
       lng: 10.75,
-      message: '<div ng-include=\"\'map/message.tpl.html\'\"></div>',
+      message: '',
       focus: false,
       draggable: false
     }
-  };
+  });
 
   $scope.$on('leafletDirectiveMarker.click', function(e, args) {
     // Args will contain the marker name and other relevant information
     console.log("Leaflet Click");
+
+    // hack : I add a template whit the shMapMessage directive in the marker message. Compilation is therefore needed to 'explain angular' that it should consider this as a directive and not simply text
     var elem = document.getElementsByClassName('leaflet-popup-content');
     $compile( elem )($scope);
   });
