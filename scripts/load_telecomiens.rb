999e92233ef3d9c8af70a9f804419b989ac2b2c7
Aurélien Hervé <mail@aurelien-herve.com>
2014-09-10 14:50:17 +0200

    ready to roll

--- scripts/load_telecomiens.rb
+++ scripts/load_telecomiens.rb
@@ -1,4 +1,4 @@
-File.open("scripts/csv/promo_2017_telecom").each_line do |line|
+File.open("scripts/csvs/promo_2017_telecom").each_line do |line|
   lastname,firstname,email = line.chomp.split("\t").map(&:strip)
-  puts SignupPermission.new(email: email, firstname: firstname, lastname: lastname, school_names: ["Telecom ParisTech"]).inspect
+  puts SignupPermission.create(email: email, firstname: firstname, lastname: lastname, school_names: ["Telecom ParisTech"])
 end
