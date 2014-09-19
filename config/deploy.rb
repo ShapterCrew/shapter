9b8d6bf14313d13bc27fb7d9b15d2cc943b178a3
Aurélien Hervé <mail@aurelien-herve.com>
2014-09-19 18:31:31 +0200

    grunt deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -77,16 +77,13 @@ namespace :deploy do
         execute "if test ! -d /usr/lib/node_modules/bower; then sudo npm install --quiet -g bower; false;fi"
         execute :sudo, :npm, :install
         execute :bower, :install
-        execute :grunt
+        execute :grunt, :deploy
       end
     end
   end
 
-  #after :publishing, :build_front
-  #after :build_front, :restart
   before :publishing, :build_front
-
   after :publishing, :restart
-  after :build_front, :delayed_jobs
+  after :restart, :delayed_jobs
 
 end
