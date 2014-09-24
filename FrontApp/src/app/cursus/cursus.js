0106106260e23431736ea415a8840e88d4dd4e6e
alexandre_herve <alexandre.herve9@gmail.com>
2014-09-24 14:53:06 +0200

    cursus initialized

---
+++ FrontApp/src/app/cursus/cursus.js
@@ -0,0 +1,27 @@
+angular.module( 'shapter.contribute', [
+  'ui.router',
+  'ui.bootstrap',
+  'security',
+  'services.appText'
+])
+
+.config(['$stateProvider', 'securityAuthorizationProvider', function config( $stateProvider, securityAuthorizationProvider ) {
+  $stateProvider.state( 'cursus', {
+    url: '/schools/:schoolId/cursus',
+    views: {
+      "main": {
+        controller: 'CursusCtrl',
+        templateUrl: 'cursus/cursus.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Cursus' },
+    resolve: {
+      authenticatedUser: securityAuthorizationProvider.requireConfirmedUser
+    }
+  });
+}])
+
+.controller('CursusCtrl', ['$scope', function( $scope ){
+}]);
+
+
