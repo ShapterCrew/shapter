a1d82eeb64591e20199f414816118b84983c983e
Aurélien Hervé <mail@aurelien-herve.com>
2014-09-26 16:13:16 +0200

    fb share-page 1ere mouture

---
+++ app/controllers/facebook_pages_controller.rb
@@ -0,0 +1,34 @@
+class FacebookPagesController < ApplicationController
+  def index
+    @permalink   = params[:permalink]
+    @title       = params[:title]
+    @type        = params[:type]
+    @description = params[:description]
+
+    @metas = {
+      "fb:app_id" => FACEBOOK_APP_TOKEN,
+      "og:site_name" => "Shapter",
+      "og:description" => params[:description],
+      "og:title" => params[:title],
+      "og:url" => @permalink,
+      "og:image" => image_meta,
+    }
+
+  end
+
+  protected
+
+  def image_meta
+    if @type == "best_comment"
+      URI.join(root_url, ActionController::Base.helpers.asset_path("logo_shapter_blue.png"))
+    elsif @type == "item"
+      URI.join(root_url, ActionController::Base.helpers.asset_path("logo_shapter_blue.png"))
+    elsif @type == "internship"
+      URI.join(root_url, ActionController::Base.helpers.asset_path("logo_shapter_blue.png"))
+    else
+      URI.join(root_url, ActionController::Base.helpers.asset_path("logo_shapter_blue.png"))
+    end
+  end
+
+
+end
