7f836087c30b883ff507ceae2579e093b44a4356
alexandre_herve <alexandre.herve9@gmail.com>
2014-09-30 10:48:05 +0200

    opening fixed jshint mixpanel conflict

--- FrontApp/src/common/security/security.js
+++ FrontApp/src/common/security/security.js
@@ -1,5 +1,6 @@
 // Based loosely around work by Witold Szczerba - https://github.com/witoldsz/angular-http-auth
 angular.module('security.service', [
+  'resources.analytics',
   'security.retryQueue',    // Keeps track of failed requests that need to be retried once the user logs in
   'security.login',         // Contains the login form template and controller
   'security.signup',
