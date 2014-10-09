5805d44f459d3d52857cf1eb3f1a7f84873e0645
Aurélien Hervé <mail@aurelien-herve.com>
2014-10-09 17:00:49 +0200

    pause

---
+++ app/mailers/daily_reporting_mailer.rb
@@ -0,0 +1,12 @@
+class DailyReportingMailer < ActionMailer::Base
+  default from: "report@shapter.com"
+
+  #def daily_new_comments_report
+  #  @comments = Item.gte(updated_at: 
+  #  mail(
+  #    to:
+  #    subject:
+  #  )
+  #end
+
+end
