25c6d7463c8d691e46fd5bb3c28f23c823e783d9
Aurélien Hervé <mail@aurelien-herve.com>
2014-10-09 14:37:58 +0200

    removed old scripts

--- scripts/limbo.rb
+++
@@ -1,5 +0,0 @@
-File.open("./lost.dat",'w') do |f|
-  User.where(provider: "facebook").where(email: /\Afake\./).where(schools: nil).each do |user|
-    f.puts [user.name, user.email].join("\t")
-  end
-end
