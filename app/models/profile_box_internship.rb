1f321541961cdf8b371db15d44f4280fba11e2f8
Aurélien Hervé <mail@aurelien-herve.com>
2014-10-14 16:11:36 +0200

    fixed internship box pb on profile

--- app/models/profile_box_internship.rb
+++ app/models/profile_box_internship.rb
@@ -3,9 +3,6 @@ class ProfileBoxInternship < ProfileBox
   belongs_to :internship
 
   validates_presence_of :internship_id
-  after_initialize :set_users
-  after_initialize :set_dates
-  after_initialize :set_name
 
   def compute_tag_ids
     internship.tags.where(category: "type").only(:id).map(&:id)
@@ -15,19 +12,20 @@ class ProfileBoxInternship < ProfileBox
     "internship"
   end
 
-  private
-
-  def set_name
-    self.name = internship.title if internship and internship.title
+  def name
+    internship.title
   end
 
-  def set_users
-    self.users = [internship.trainee] if internship and internship.trainee
+  def users
+    internship.trainees
   end
 
-  def set_dates
-    self.start_date = internship.start_date if internship and internship.start_date
-    self.end_date = internship.end_date if internship and internship.end_date
+  def start_date
+    internship.start_date
+  end
+
+  def end_date
+    internship.end_date
   end
 
 end
