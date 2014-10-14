9af5146f5cca9993abda4eb9a2ac1f312f9f7dd4
Aurélien Hervé <mail@aurelien-herve.com>
2014-10-14 16:30:34 +0200

    DEDIEU!

--- app/models/profile_box_internship.rb
+++ app/models/profile_box_internship.rb
@@ -3,6 +3,7 @@ class ProfileBoxInternship < ProfileBox
   belongs_to :internship
 
   validates_presence_of :internship_id
+  before_validation :save_floating_attributes!
 
   def compute_tag_ids
     internship.tags.where(category: "type").only(:id).map(&:id)
@@ -26,6 +27,16 @@ class ProfileBoxInternship < ProfileBox
 
   def end_date
     internship.end_date
+  end
+
+  private
+  def save_floating_attributes!
+    self.user_ids = internship.trainee_ids
+    users.each{|u| u.update_attribute(:profile_box_ids, (u.profile_box_ids << self.id).uniq)}
+    self.name = name
+    self.type = type
+    self.start_date = start_date
+    self.end_date = end_date
   end
 
 end
